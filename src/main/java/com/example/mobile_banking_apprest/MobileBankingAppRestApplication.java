af4aa8f3d1b0560f030c22b0c4d808c0a805193d
messer82 <podinamarius82@gmail.com>
2020-05-21 13:57:30 +0300

    Initial commit

---
+++ src/main/java/com/example/mobile_banking_apprest/MobileBankingAppRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.mobile_banking_apprest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileBankingAppRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileBankingAppRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mobile_banking_apprest/MobileBankingAppRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mobile_banking_apprest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileBankingAppRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
